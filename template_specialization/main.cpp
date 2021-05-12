e70a2189c91a6677cac4c6d60791ccb4e8a2dbd0
beliaevs <beliaevs@gmail.com>
2021-05-13 01:12:28 +0300

    template overloading

--- template_specialization/main.cpp
+++ template_specialization/main.cpp
@@ -2,15 +2,20 @@
 
 
 template<typename T1, typename T2>
-void f(T1 t1, T2 t2)
+void f(T1, T2)
 {
-    std::cout << "Root template\n";
+    std::cout << "T1, T2\n";
 }
 
 template<typename T>
-void f(T t1, T t2)
+void f(T, T)
 {
     std::cout << "equal types\n";
+}
+
+void f(int, int)
+{
+    std::cout << "int version\n";
 }
 
 
@@ -18,5 +23,6 @@ int main()
 {
     f(1, 2);
     f(1., 2);
+    f(1., 2.);
     return 0;
 }
