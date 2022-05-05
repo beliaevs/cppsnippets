a4a319c97cf9cb96d54f8d68b2f87daaacdc40ee
Alexander Beliaev <beliaevs@gmail.com>
2022-05-05 19:30:36 +0300

    clang-formatted with Shift-Alt-F

--- ub/main.cpp
+++ ub/main.cpp
@@ -1,36 +1,36 @@
-#include<iostream>
-#include<limits>
+#include <iostream>
+#include <limits>
 
 bool foo(int x)
 {
-    std::cout << x << ", " << x + 1 << ": ";
-    return x+1 > x;
+  std::cout << x << ", " << x + 1 << ": ";
+  return x + 1 > x;
 }
 
 bool exists_in_table(int v)
 {
-    static int table[4] = {1,3,2,4};
-    constexpr int N = 10;
-    for(int i = 0; i <= N; i++)
+  static int table[4] = { 1, 3, 2, 4 };
+  constexpr int N = 10;
+  for (int i = 0; i <= N; i++)
+  {
+    std::cout << table[i] << "\n";
+    if (table[i] == v)
     {
-        std::cout << table[i] << "\n";
-        if(table[i] == v)
-        {
-            return true;
-        }
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 int main()
 {
-    std::cout << "foo:\n";
-    std::cout << foo(1) << "\n" << foo(std::numeric_limits<int>::max()) << "\n";
-    std::cout << "table:\n";
-    for(auto i: {3, 5})
-    {
-        std::cout << exists_in_table(i) << "\n";
-    }
-    
-    return 0;
+  std::cout << "foo:\n";
+  std::cout << foo(1) << "\n" << foo(std::numeric_limits<int>::max()) << "\n";
+  std::cout << "table:\n";
+  for (auto i : { 3, 5 })
+  {
+    std::cout << exists_in_table(i) << "\n";
+  }
+
+  return 0;
 }
