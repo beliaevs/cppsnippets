a07cd3081ef21a9de001bb906982e7b768d53c9e
Alexander Beliaev <beliaevs@gmail.com>
2022-05-12 15:29:01 +0300

    fancy table

--- formatted_output/formats.cpp
+++ formatted_output/formats.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+#include <cmath>
 #include <iomanip>
+#include <iostream>
 
 int main()
 {
@@ -10,7 +11,26 @@ int main()
 
     const int prec = 12;
     std::cout << "setting precision to " << prec << "\n";
-    std::cout << "default: " << std::defaultfloat << std::setprecision(prec) << quarter << ", scientific: " << std::scientific << quarter
-              << ", hexfloat: " << std::hexfloat << quarter << ", fixed: " << std::fixed << quarter << "\n";
+    std::cout << "default: " << std::defaultfloat << std::setprecision(prec) << quarter
+              << ", scientific: " << std::scientific << quarter << ", hexfloat: " << std::hexfloat << quarter
+              << ", fixed: " << std::fixed << quarter << "\n";
+  }
+  {
+    // fancy table
+    // 11 - 16 - 23
+    constexpr int col1 = 11;
+    constexpr int col2 = 16;
+    constexpr int col3 = 23;
+    std::cout << "------------------------------------------------------\n";
+    std::cout << "|12345678901|1234567890123456|12345678901234567890123|\n";
+    std::cout << "|" << std::left << std::setw(col1) << col1 << "|" << std::right << std::setw(col2) << col2 << "|"
+              << std::setw(col3) << col3 << "|\n";
+    std::cout << "|" << std::left << std::setw(col1) << "sqrt(2)"
+              << "|" << std::setw(col2) << std::right << std::sqrt(2.) << "|" << std::setw(col3)
+              << std::setprecision(18) << std::sqrt(2.) << "|\n";
+    std::cout << "|" << std::setw(col1) << std::left << "sqrt(3)"
+              << "|" << std::setw(col2) << std::setprecision(12) << std::right << std::sqrt(3.) << "|"
+              << std::setw(col3) << std::setprecision(18) << std::sqrt(3.) << "|\n";
+    std::cout << "------------------------------------------------------\n";
   }
 }
