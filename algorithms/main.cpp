026ef3558b08f43c18caf8a762e86eb6cc636776
Alexander Beliaev <beliaevs@gmail.com>
2022-05-17 14:18:46 +0300

    STL algorithms testing

---
+++ algorithms/main.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <iostream>
+
+int main()
+{
+  // lambda utilities
+  auto printSeq = [](auto&& seq) {
+    for (auto&& e : seq)
+    {
+      std::cout << e << " ";
+    }
+    std::cout << "\n";
+  };
+
+  // testing various algorithm groups
+  {
+    // heaps
+    std::vector<int> v = { 2, 7, 1, 9, 2, 4, 0, 5, 3, 6, 8 };
+    std::cout << "initial:\n";
+    printSeq(v);
+    std::cout << "make_heap:\n";
+    make_heap(v.begin(), v.end());
+    printSeq(v);
+    for (auto it = v.end(); it != v.begin(); --it)
+    {
+      pop_heap(v.begin(), it);
+      std::cout << "pop_heap:\n";
+      printSeq(v);
+    }
+  }
+
+  {
+    // sorted sequences
+    std::vector<int> v = { 1, 3, 5, 7, 2, 4, 6, 8 };
+    std::cout << "before merge:\n";
+    printSeq(v);
+    std::inplace_merge(v.begin(), v.begin() + 4, v.end());
+    std::cout << "after merge:\n";
+    printSeq(v);
+  }
+  return 0;
+}
