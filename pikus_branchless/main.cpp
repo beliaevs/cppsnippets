9f60e240a9f1de9f4ba548292ec63cfd15d66952
Alexander Beliaev <beliaevs@gmail.com>
2022-05-18 17:37:56 +0300

    StopWatch

--- pikus_branchless/main.cpp
+++ pikus_branchless/main.cpp
@@ -3,6 +3,29 @@
 #include <vector>
 #include <cmath>
 
+class StopWatch
+{
+public:
+  StopWatch()
+  {
+    reset();
+  }
+
+  void reset()
+  {
+    d_time = std::chrono::high_resolution_clock::now();
+  }
+
+  auto microseconds() const
+  {
+    return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - d_time)
+      .count();
+  }
+
+private:
+  std::chrono::high_resolution_clock::time_point d_time;
+};
+
 void init(std::vector<double>& x, double seed)
 {
   double s = 0.;
@@ -24,30 +47,28 @@ int main()
   init(x, 0.1);
   init(y, 0.2);
   {
-    auto start = std::chrono::system_clock::now();
+    StopWatch t;
     double acc1 = 0.;
     for (int i = 0; i != N; ++i)
     {
       acc1 += x[i] * y[i];
     }
-    auto stop = std::chrono::system_clock::now();
 
-    std::cout << "time elapsed:" << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count()
-              << "ms\n";
+    auto res = t.microseconds();
+
+    std::cout << "time elapsed: " << res << "[ms]\n";
     std::cout << "acc1 = " << acc1 << "\n";
   }
   {
-    auto start = std::chrono::system_clock::now();
+    StopWatch t;
     double acc1 = 0., acc2 = 0.;
     for (int i = 0; i != N; ++i)
     {
       acc1 += x[i] * y[i];
       acc2 += (x[i] + y[i]) * (x[i] - y[i]);
     }
-    auto stop = std::chrono::system_clock::now();
-
-    std::cout << "time elapsed:" << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count()
-              << "ms\n";
+    auto res = t.microseconds();
+    std::cout << "time elapsed: " << res << "[ms]\n";
     std::cout << "acc1 = " << acc1 << ", acc2 = " << acc2 << "\n";
   }
   return 0;
